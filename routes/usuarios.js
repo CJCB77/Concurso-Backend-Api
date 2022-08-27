53adab5bbef039a49a88eda051ba9d19af2ef0ed
Carlos Coral <coral.johnny.2@gmail.com>
2022-08-27 00:54:04 -0500

    Cambie rutas delete y update usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,7 @@ const usuarioController = require('../controllers/usuariosController');
 
 router.get('/', usuarioController.getUsuarios);
 router.get('/:id', usuarioController.getUsuario);
-router.put('/:id', usuarioController.updateUsuario);
-router.delete('/:id', usuarioController.deleteUsuario);
+router.put('/update/:id', usuarioController.updateUsuario);
+router.delete('/delete/:id', usuarioController.deleteUsuario);
 
 module.exports = router;

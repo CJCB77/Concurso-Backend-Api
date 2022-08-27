45f3d112853ea42e6e4ef7ff1ed1870aa143d922
Carlos Coral <coral.johnny.2@gmail.com>
2022-08-27 00:47:34 -0500

    Arregle tabla usuario

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -2,7 +2,7 @@ const db = require('../db/connection');
 
 const getUsuarios = async (req, res) => {
     try{
-        const query = `SELECT * FROM usuarios`;
+        const query = `SELECT * FROM usuario`;
         const result = await db.query(query);
         res.json(result.rows);
     }catch(error){
@@ -14,7 +14,7 @@ const getUsuarios = async (req, res) => {
 const getUsuario = async (req, res) => {
     try{
         const {id} = req.params;
-        const query = `SELECT * FROM usuarios WHERE id = $1`;
+        const query = `SELECT * FROM usuario WHERE id = $1`;
         const result = await db.query(query, [id]);
         res.json(result.rows[0]);
     }catch(error){
@@ -27,7 +27,7 @@ const updateUsuario = async (req, res) => {
     try{
         const {id} = req.params;
         const {username,password,rol} = req.body;
-        const query = `UPDATE usuarios SET username = $1, password = $2, 
+        const query = `UPDATE usuario SET username = $1, password = $2, 
             rol = $3 WHERE id = $4 RETURNING *`;
         const result = await db.query(query, [username,password,rol,id]);
         res.json(result.rows[0]);
@@ -40,7 +40,7 @@ const updateUsuario = async (req, res) => {
 const deleteUsuario = async (req, res) => {
     try{
         const {id} = req.params;
-        const query = `DELETE FROM usuarios WHERE id = $1 RETURNING *`;
+        const query = `DELETE FROM usuario WHERE id = $1 RETURNING *`;
         const result = await db.query(query, [id]);
         res.json(result.rows[0]);
     }catch(error){
